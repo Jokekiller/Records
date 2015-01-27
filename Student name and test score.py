119e980ddce78fde133ac633d4f3f91c3e58c6ae
Harry Robinson <41194@longroad.ac.uk>
2015-01-27 15:18:24 +0000

    Student name

--- Student name and test score.py
+++ Student name and test score.py
@@ -5,19 +5,19 @@ class studentMarks:
     def __init__(self):
         self.studentName = None
         self.score = None
-name = studentMarks()
-for count in range(4):    
-    name.studentName = input("Enter the name of the student: ")
-    name.score = int(input("Enter the score the student got: "))
 
 names = []
 for count in range(4):
-    names.append(studentMarks)
-    count = count + 1
+    name = studentMarks()
+    name.studentName = input("Enter the name of the student: ")
+    name.score = int(input("Enter the score the student got: "))
+    names.append(name)
+
+#names = []
+#for count in range(4):
+#    names.append(studentMarks())
 
 print("|Student Name|Score|")
-for index in range (4):
-    print("|{0:^12}|{1:^5}|".format(name.studentName, name.score))
-    index = index + 1
-    print(names[index].number)
+for name in names:
+    print("{0:^6}{1:^8}".format(name.studentName, name.score))
     
