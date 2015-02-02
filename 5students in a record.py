b77b8f1aa52ea91dc0188e76adfe4a6c5835e3ed
Harry Robinson <41194@longroad.ac.uk>
2015-02-02 12:18:03 +0000

    Students in a record

--- 5students in a record.py
+++ 5students in a record.py
@@ -9,20 +9,33 @@ class student():
         self.modules = None
 
 student_info = []
-for count in range (5):
+for count in range (2):
     info = student()
     info.modules = []
     info.marks = []
     info.name = input("Enter the students Name: ")
-    for score in range (4):
+    for score in range (2):
         module = input("Enter the module that student took: ")
         info.modules.append(module)
         mark = int(input("Enter the score they got on that module: "))
-        info.marks.append(mark)
+        info.marks.append(mark) 
     student_info.append(info)
+total = 0
+for mark in info.marks:
+    total = total + mark
+    meanMark = total / 2
+print("Mean mark is {0}".format(meanMark))
 print("|Student Name| Modules | Mark|")
 print("-"*30)
 for info in student_info:
     print("|{0:<12}|".format(info.name))
-    for index in range(4):
-            print("             |{0:<9}|{1:<6}|".format(info.modules[index], info.marks[index]))
+    studentTotal = 0
+    for index in range(2):
+        print("             |{0:<9}|{1:<6}|".format(info.modules[index], info.marks[index]))
+        studentTotal =  (studentTotal + info.marks[index]) / 2
+    if studentTotal > meanMark:
+        differenceMark = studentTotal - meanMark
+        print("Total mean mark is {0}, you were {1} above the mean mark {2}".format(studentTotal, differenceMark, meanMark))
+    elif meanMark > studentTotal:
+        differenceMark = meanMark - studentTotal
+        print("Total mean mark is {0}, you were {1} below the mean mark {2}".format(studentTotal, differenceMark, meanMark))
